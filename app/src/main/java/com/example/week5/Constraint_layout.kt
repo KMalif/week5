65d91997276df3e41639c21b1d41ce2d381df55d
KMalif <khafidulmualif2019@gmail.com>
2021-01-05 21:00:15 +0700

    initial commit

---
+++ app/src/main/java/com/example/week5/Constraint_layout.kt
@@ -0,0 +1,11 @@
+package com.example.week5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Constraint_layout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout)
+    }
+}
